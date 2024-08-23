5693089d3cfa115a571c28bf17f344e81ad72f80
Curso003 <correo@correo.co>
2024-08-23 11:38:52 +0200

    Version funcional 1

--- Back/Servejerc12/src/main/java/es/cic/grupo1/Servejerc12/config/CORSConfig.java
+++ Back/Servejerc12/src/main/java/es/cic/grupo1/Servejerc12/config/CORSConfig.java
@@ -13,7 +13,7 @@ public class CORSConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
             }
         };
     }
