02eeedcdc31b8b49ab8065f0ed56acfae74909b3
Curso003 <correo@correo.co>
2024-08-23 11:02:03 +0200

    prueba de conectar front y back

--- Front/interface/src/App.vue
+++ Front/interface/src/App.vue
@@ -1,9 +1,31 @@
-<script setup>
-
-</script>
-
 <template>
-
-</template>
-
-
+    <div id="app">
+      <router-link to="/data">Data Table</router-link>
+      <router-link to="/details">Detail Table</router-link>
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  router-link {
+    margin: 0 10px;
+    text-decoration: none;
+    color: #42b983;
+  }
+  
+  router-link-active {
+    font-weight: bold;
+  }
+  </style>
--- Front/interface/src/router/index.js
+++ Front/interface/src/router/index.js
@@ -1,23 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import DataTable from '../components/DataTable.vue';
+import DetailTable from '../components/DetailTable.vue';
+
+const routes = [
+  { path: '/', redirect: '/data' },
+  { path: '/data', name: 'DataTable', component: DataTable },
+  { path: '/details', name: 'DetailTable', component: DetailTable }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes
+});
 
-export default router
+export default router;
