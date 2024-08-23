8252a5c8b1de3dc7bde842f60fa2a509cadb9b38
curso10 <curso10@nomail.com>
2024-08-23 10:35:46 +0200

    BackEnd

---
+++ Back/Servejerc12/src/main/java/es/cic/grupo1/Servejerc12/Servejerc12Application.java
@@ -0,0 +1,13 @@
+package es.cic.grupo1.Servejerc12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servejerc12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servejerc12Application.class, args);
+	}
+
+}
---
+++ Back/Servejerc12/src/test/java/es/cic/grupo1/Servejerc12/Servejerc12ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.grupo1.Servejerc12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servejerc12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
