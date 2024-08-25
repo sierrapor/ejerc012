272b276be4a1f65fe1f58b3c24ffe286589b7950
Diego <mi@email.com>
2024-08-26 00:41:57 +0200

    Test cypress

--- Front/cypress/e2e/example.cy.js
+++ Front/cypress/e2e/example.cy.js
@@ -1,8 +1,24 @@
-// https://on.cypress.io/api
+describe('App Navigation and Content', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
-  })
-})
+  it('should display the welcome message on the home page', () => {
+    cy.contains('¡Bienvenido a la página de los coches!').should('be.visible');
+  });
+
+  it('should navigate to the Coches page and display the table', () => {
+    cy.contains('Coches').click();
+    cy.url().should('include', '/coches');
+    cy.get('table').should('be.visible');
+    cy.get('th').contains('Fabricante').should('be.visible');
+    cy.get('th').contains('Modelo').should('be.visible');
+    cy.get('th').contains('Año').should('be.visible');
+  });
+
+  it('should display car data in the table', () => {
+    cy.contains('Coches').click();
+    cy.url().should('include', '/coches');
+    cy.get('tbody tr').should('have.length.greaterThan', 0);
+  });
+});
