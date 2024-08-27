a484389a127b54796edf53cbc4a449eb7d0c597f
Curso003 <correo@correo.co>
2024-08-27 09:26:43 +0200

    cambios menores

--- Front/src/views/Inicio.vue
+++ Front/src/views/Inicio.vue
@@ -33,9 +33,7 @@ body, html {
 
 .inicio-container {
     padding: 20px;
-    background-color: #f9f9f9;
     border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     text-align: center;
     padding-bottom: 25vh;
 }
